a4e86bd4a7eb7ed57378a43cfc1a56c63d9f2878
yuanx25 <yuanx25@163.com>
2021-08-02 15:29:04 +0800

    login+logout

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -17,4 +17,14 @@ public interface CommunityConstant {
      */
     int ACTIVATION_FAILURE = 2;
 
+    /**
+     * 默认状态的登录凭证超市时间
+     */
+    int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
+
+    /**
+     * 记住状态下的登录凭证超市时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
+
 }
