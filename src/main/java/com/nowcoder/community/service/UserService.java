0fc772fd914c6f96b5b7a1f387199018dee05f5d
yuanx25 <yuanx25@163.com>
2021-08-03 15:12:41 +0800

    账号设置，修改密码

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -163,4 +163,44 @@ public class UserService implements CommunityConstant {
         return loginTicketMapper.selectByTicket(ticket);
     }
 
+    public int updateHeader(int userId, String headerUrl) {
+        return userMapper.updateHeader(userId, headerUrl);
+    }
+
+    //修改密码
+    public Map<String, Object> updatePassword(int userId, String oldPassword, String newPassword, String confirmPassword) {
+        Map<String, Object> map = new HashMap<>();
+        //空值处理
+        if(StringUtils.isBlank(oldPassword)) {
+            map.put("oldPasswordMsg", "原密码不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank(newPassword)) {
+            map.put("newPasswordMsg", "新密码不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank(confirmPassword)) {
+            map.put("confirmPasswordMsg", "新密码不能为空！");
+            return map;
+        }
+
+        //验证原始密码
+        User user = userMapper.selectById(userId);
+        oldPassword = CommunityUtil.md5(oldPassword + user.getSalt());
+        if(!user.getPassword().equals(oldPassword)) {
+            map.put("oldPasswordMsg", "原密码输入有误");
+            return map;
+        }
+
+        if(!newPassword.equals(confirmPassword)) {
+            map.put("confirmPasswordMsg", "两次密码输入不一致！");
+            return map;
+        }
+
+        //更新密码
+        newPassword = CommunityUtil.md5(newPassword + user.getSalt());
+        userMapper.updatePassword(userId, newPassword);
+        return map;
+    }
+
 }
